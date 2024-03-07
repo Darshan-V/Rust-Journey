b8df300bacef20198b8b980f1d8266ebd0017d60
Darshan-V <mr.darshanv@gmail.com>
2024-03-08 01:36:37 +0530

    first tcp/http server in rust

---
+++ todo-backend/src/main.rs
@@ -0,0 +1,50 @@
+use std::{io::{prelude::*, BufReader}, net::{TcpListener, TcpStream}};
+
+fn main() {
+    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+
+        handle_connection(stream);
+    }
+}
+
+// in the main function, we are binding the server to the address and port,
+// then we are listening for incoming connections.
+// When a connection is received, we are calling the handle_connection function and passing the stream to it.
+
+
+fn handle_connection(mut stream: TcpStream) {
+    let buf_reader = BufReader::new(&mut stream);
+    let http_request: Vec<_> = buf_reader
+        .lines()
+        .map(|result| result.unwrap())
+        .take_while(|line| !line.is_empty())
+        .collect();
+
+    println!("Request: {:#?}", http_request);
+}
+
+
+/*
+Request: [
+    "GET / HTTP/1.1",
+    "Host: 127.0.0.1:7878",
+    "Connection: keep-alive",
+    "Pragma: no-cache",
+    "Cache-Control: no-cache",
+    "sec-ch-ua: \"Chromium\";v=\"122\", \"Not(A:Brand\";v=\"24\", \"Google Chrome\";v=\"122\"",
+    "sec-ch-ua-mobile: ?0",
+    "sec-ch-ua-platform: \"macOS\"",
+    "Upgrade-Insecure-Requests: 1",
+    "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36",
+    "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,;q=0.8,application/signed-exchange;v=b3;q=0.7",
+    "Sec-Fetch-Site: cross-site",
+    "Sec-Fetch-Mode: navigate",
+    "Sec-Fetch-User: ?1",
+    "Sec-Fetch-Dest: document",
+    "Accept-Encoding: gzip, deflate, br, zstd",
+    "Accept-Language: en-GB,en-US;q=0.9,en;q=0.8",
+]
+ */
